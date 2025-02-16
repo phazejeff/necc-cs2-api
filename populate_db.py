e2b56cde0c7b1d5d99364c9cff647b59937215a0
phazejeff <85849384+phazejeff@users.noreply.github.com>
2025-02-16 12:31:32 -0800

    error on match will continue running and just print error

--- populate_db.py
+++ populate_db.py
@@ -8,6 +8,7 @@ from database import database
 import os
 import json
 from peewee import DoesNotExist
+import traceback
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
@@ -44,75 +45,79 @@ for tournament_id in TOURNAMENT_IDS:
     while len(matches) != 0:
         print(f"Running offset {offset}")
         for match in matches:
-            try: 
-                Match.get_by_id(match.get("match_id"))
-                continue
-            except DoesNotExist:
-                pass
-
-            team1: dict = match.get("teams").get("faction1")
-            team1_db = Team.initialize(match, team1, division_num)
-            team2: dict = match.get("teams").get("faction2")
-            team2_db = Team.initialize(match, team2, division_num)
-
-            try: 
-                Team.get_by_id(team1_db.team_id)
-            except DoesNotExist:
-                if team1_db not in teams_db:
-                    teams_db.append(team1_db)
-            
-            try: 
-                Team.get_by_id(team2_db.team_id)
-            except DoesNotExist:
-                if team2_db not in teams_db:
-                    teams_db.append(team2_db)
-
-            match_db = Match.initialize(match, team1, team2)
-
-            if match_db not in matches_db:
-                matches_db.append(match_db)
-
-            if team1_db.name == 'bye' or team2_db.name == 'bye':
-                continue
-
-            for player in team1.get("roster"):
-                player: dict
-                player_db = Player.initialize(player, team1)
+            try:
                 try: 
-                    Player.get_by_id(player_db.player_id)
+                    Match.get_by_id(match.get("match_id"))
+                    continue
                 except DoesNotExist:
-                    if player_db not in players_db:
-                        players_db.append(player_db)
+                    pass
 
-            for player in team2.get("roster"):
-                player: dict
-                player_db = Player.initialize(player, team2)
+                team1: dict = match.get("teams").get("faction1")
+                team1_db = Team.initialize(match, team1, division_num)
+                team2: dict = match.get("teams").get("faction2")
+                team2_db = Team.initialize(match, team2, division_num)
+
                 try: 
-                    Player.get_by_id(player_db.player_id)
+                    Team.get_by_id(team1_db.team_id)
                 except DoesNotExist:
-                    if player_db not in players_db:
-                        players_db.append(player_db)
-            
-            match_stats: dict = faceit.get_match_stats(match.get("match_id"))
-            rounds = match_stats.get("rounds")
-            if rounds == None:
-                continue
+                    if team1_db not in teams_db:
+                        teams_db.append(team1_db)
+                
+                try: 
+                    Team.get_by_id(team2_db.team_id)
+                except DoesNotExist:
+                    if team2_db not in teams_db:
+                        teams_db.append(team2_db)
 
-            for map in match_stats.get("rounds"):
-                map: dict
-                map_db = Map.initialize(map, match)
+                match_db = Match.initialize(match, team1, team2)
 
-                if map_db not in maps_db:
-                    maps_db.append(map_db)
+                if match_db not in matches_db:
+                    matches_db.append(match_db)
 
-                teams: list[dict] = map.get("teams")
-                for team in teams:
-                    players: list[dict] = team.get("players")
-                    for player in players:
-                        playerstat_db = PlayerStat.initialize(player, map_db)
-                        
-                        if playerstat_db not in playerstats_db:
-                            playerstats_db.append(playerstat_db)
+                if team1_db.name == 'bye' or team2_db.name == 'bye':
+                    continue
+
+                for player in team1.get("roster"):
+                    player: dict
+                    player_db = Player.initialize(player, team1)
+                    try: 
+                        Player.get_by_id(player_db.player_id)
+                    except DoesNotExist:
+                        if player_db not in players_db:
+                            players_db.append(player_db)
+
+                for player in team2.get("roster"):
+                    player: dict
+                    player_db = Player.initialize(player, team2)
+                    try: 
+                        Player.get_by_id(player_db.player_id)
+                    except DoesNotExist:
+                        if player_db not in players_db:
+                            players_db.append(player_db)
+                
+                match_stats: dict = faceit.get_match_stats(match.get("match_id"))
+                rounds = match_stats.get("rounds")
+                if rounds == None:
+                    continue
+
+                for map in match_stats.get("rounds"):
+                    map: dict
+                    map_db = Map.initialize(map, match)
+
+                    if map_db not in maps_db:
+                        maps_db.append(map_db)
+
+                    teams: list[dict] = map.get("teams")
+                    for team in teams:
+                        players: list[dict] = team.get("players")
+                        for player in players:
+                            playerstat_db = PlayerStat.initialize(player, map_db)
+                            
+                            if playerstat_db not in playerstats_db:
+                                playerstats_db.append(playerstat_db)
+            except Exception as e:
+                print(f"Match {match.get("match_id")} failed.")
+                traceback.print_exc()
 
         offset += pageAmount
         matches: list[dict] = faceit.get_championship_matches(tournament_id, "past", pageAmount, offset)
