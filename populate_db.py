7eea834639d8ae39b91c6c4f43c413d56cdf8fb1
phazejeff <85849384+phazejeff@users.noreply.github.com>
2025-03-07 12:38:56 -0800

    updated populate_db to overwrite remade matches

--- populate_db.py
+++ populate_db.py
@@ -47,30 +47,35 @@ for tournament_id in TOURNAMENT_IDS:
         print(f"Running offset {offset}")
         for match in matches:
             try:
-                try: 
-                    Match.get_by_id(match.get("match_id"))
-                    continue
-                except DoesNotExist:
-                    pass
-
                 team1: dict = match.get("teams").get("faction1")
                 team1_db = Team.initialize(match, team1, division_num)
                 team2: dict = match.get("teams").get("faction2")
                 team2_db = Team.initialize(match, team2, division_num)
 
-                try:
-                    teams_db.remove(team1_db)
-                except:
-                    pass
-                teams_db.append(team1_db)
+                try: 
+                    team_in_db: Team = Team.get_by_id(team1_db.team_id)
+                    if team_in_db.__data__ != team1_db.__data__:
+                        teams_db.append(team1_db)
+                except DoesNotExist:
+                    if team1_db not in teams_db:
+                        teams_db.append(team1_db)
             
-                try:
-                    teams_db.remove(team2_db)
-                except:
-                    pass
-                teams_db.append(team2_db)
+                try: 
+                    team_in_db: Team = Team.get_by_id(team2_db.team_id)
+                    if team_in_db.__data__ != team2_db.__data__:
+                        teams_db.append(team2_db)
+                except DoesNotExist:
+                    if team2_db not in teams_db:
+                        teams_db.append(team2_db)
 
                 match_db = Match.initialize(match, team1, team2)
+
+                try: 
+                    match_in_db: Match = Match.get_by_id(match.get("match_id"))
+                    if match_db.__data__ == match_in_db.__data__:
+                        continue
+                except DoesNotExist:
+                    pass
 
                 if match_db not in matches_db:
                     matches_db.append(match_db)
@@ -81,20 +86,24 @@ for tournament_id in TOURNAMENT_IDS:
                 for player in team1.get("roster"):
                     player: dict
                     player_db = Player.initialize(player, team1)
-                    try:
-                        players_db.remove(player_db)
-                    except:
-                        pass
-                    players_db.append(player_db)
+                    try: 
+                        player_in_db: Player = Player.get_by_id(player_db.player_id)
+                        if player_in_db.__data__ != player_db.__data__:
+                            players_db.append(player_db)
+                    except DoesNotExist:
+                        if player_db not in players_db:
+                            players_db.append(player_db)
 
                 for player in team2.get("roster"):
                     player: dict
                     player_db = Player.initialize(player, team2)
-                    try:
-                        players_db.remove(player_db)
-                    except:
-                        pass
-                    players_db.append(player_db)
+                    try: 
+                        player_in_db: Player = Player.get_by_id(player_db.player_id)
+                        if player_in_db.__data__ != player_db.__data__:
+                            players_db.append(player_db)
+                    except DoesNotExist:
+                        if player_db not in players_db:
+                            players_db.append(player_db)
                 
                 match_stats: dict = faceit.get_match_stats(match.get("match_id"))
                 rounds = match_stats.get("rounds")
@@ -104,18 +113,26 @@ for tournament_id in TOURNAMENT_IDS:
                 for map in match_stats.get("rounds"):
                     map: dict
                     map_db = Map.initialize(map, match)
-
-                    if map_db not in maps_db:
-                        maps_db.append(map_db)
+                    try: 
+                        map_in_db: Map = Map.get_by_id(map_db.map_id)
+                        if map_in_db.__data__ != map_db.__data__:
+                            maps_db.append(maps_db)
+                    except DoesNotExist:
+                        if map_db not in maps_db:
+                            maps_db.append(maps_db)
 
                     teams: list[dict] = map.get("teams")
                     for team in teams:
                         players: list[dict] = team.get("players")
                         for player in players:
-                            playerstat_db = PlayerStat.initialize(player, map_db)
-                            
-                            if playerstat_db not in playerstats_db:
-                                playerstats_db.append(playerstat_db)
+                            playerstat_db: PlayerStat = PlayerStat.initialize(player, map_db)
+                            try: 
+                                playerstat_in_db: PlayerStat = PlayerStat.get_by_id(playerstat_db.player)
+                                if playerstat_in_db.__data__ != playerstat_db.__data__:
+                                    playerstats_db.append(playerstat_db)
+                            except DoesNotExist:
+                                if playerstat_db not in playerstats_db:
+                                    playerstats_db.append(playerstat_db)
             except Exception as e:
                 print(f"Match {match.get("match_id")} failed.")
                 traceback.print_exc()
@@ -123,11 +140,21 @@ for tournament_id in TOURNAMENT_IDS:
         offset += pageAmount
         matches: list[dict] = faceit.get_championship_matches(tournament_id, "past", pageAmount, offset)
 
+def chunked_iterator(iterable, size):
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
+batch_size = 500
 with database.atomic():
-    Team.replace_many([obj.__data__ for obj in teams_db]).execute()
-    Player.replace_many([obj.__data__ for obj in players_db]).execute()
-    Match.replace_many([obj.__data__ for obj in matches_db]).execute()
-    Map.replace_many([obj.__data__ for obj in maps_db]).execute()
-    PlayerStat.replace_many([obj.__data__ for obj in playerstats_db]).execute()
+    for batch in chunked_iterator(teams_db, batch_size):
+        Team.replace_many([obj.__data__ for obj in batch]).execute()
+    for batch in chunked_iterator(players_db, batch_size):
+        Player.replace_many([obj.__data__ for obj in batch]).execute()
+    for batch in chunked_iterator(matches_db, batch_size):
+        Match.replace_many([obj.__data__ for obj in batch]).execute()
+    for batch in chunked_iterator(maps_db, batch_size):
+        Map.replace_many([obj.__data__ for obj in batch]).execute()
+    for batch in chunked_iterator(playerstats_db, batch_size):
+        PlayerStat.replace_many([obj.__data__ for obj in batch]).execute()
 
 print("Done.")
